7a43f56506b34073c2db9316ccda4ac2564e7383
Raul Gonçalves <raulsantos121@gmail.com>
2023-06-11 17:13:33 -0300

    Adicionando PageJsonSerializer

---
+++ src/main/java/com/algaworks/algafood/core/jackson/PageJsonSerializer.java
@@ -0,0 +1,26 @@
+package com.algaworks.algafood.core.jackson;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import org.springframework.boot.jackson.JsonComponent;
+import org.springframework.data.domain.Page;
+
+import java.io.IOException;
+
+@JsonComponent //JsonComponent - Componente q fornece implementação de serializador/desserializador q deve ser registrado no jackson
+public class PageJsonSerializer extends JsonSerializer<Page<?>> {  //Page<?> - serializador para qualquer tipo de página
+
+	@Override		//Alterando a serialização da paginação dos endpoints
+	public void serialize(Page<?> page, JsonGenerator generator, SerializerProvider serializers) throws IOException {
+		generator.writeStartObject();
+		//Adicionando as informações desejadas para o Page
+		generator.writeObjectField("content", page.getContent());
+		generator.writeNumberField("size", page.getSize());
+		generator.writeNumberField("totalElements", page.getTotalElements());
+		generator.writeNumberField("totalPages", page.getTotalPages());
+		generator.writeNumberField("number", page.getNumber());
+		
+		generator.writeEndObject();
+	}
+}
