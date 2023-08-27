d3ce00360382d5ce31d512249eef15096237baa8
Raul Gonçalves <raulsantos121@gmail.com>
2023-08-27 17:34:27 -0300

    Adicionando configurações para capturar chave pública de uma criptografia assimétrica

--- src/main/java/com/algaworks/algafood/core/security/ResourceServerConfig.java
+++ src/main/java/com/algaworks/algafood/core/security/ResourceServerConfig.java
@@ -1,14 +1,9 @@
 package com.algaworks.algafood.core.security;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.oauth2.jwt.JwtDecoder;
-import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
-
-import javax.crypto.spec.SecretKeySpec;
 
 @Configuration
 @EnableWebSecurity
@@ -23,11 +18,10 @@ public class ResourceServerConfig extends WebSecurityConfigurerAdapter {
 			.oauth2ResourceServer().jwt();
 	}
 
-	@Bean
-	public JwtDecoder jwtDecoder() {
-		//1 arg é um array de bytes e o 2 arg é o algoritmo para a chave secreta
-		var secretKey = new SecretKeySpec("89a7sd89f7as98f7dsa98fds7fd89sasd9898asdf98s".getBytes(), "HmacSHA256");
-
-		return NimbusJwtDecoder.withSecretKey(secretKey).build();
-	}
+//	@Bean		//Chave secreta simétrica
+//	public JwtDecoder jwtDecoder() {
+//		//1 arg é um array de bytes e o 2 arg é o algoritmo para a chave secreta
+//		var secretKey = new SecretKeySpec("89a7sd89f7as98f7dsa98fds7fd89sasd9898asdf98s".getBytes(), "HmacSHA256");
+//		return NimbusJwtDecoder.withSecretKey(secretKey).build();
+//	}
 }
