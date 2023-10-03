3935eb73001fc1f06a5902723d8e41283ed7dacb
Raul Gonçalves <raulsantos121@gmail.com>
2023-10-02 22:10:14 -0300

    Resolvendo problema de scopo do Client Credentials

--- src/main/java/com/algaworks/algafood/core/config/RedisConfig.java
+++ src/main/java/com/algaworks/algafood/core/config/RedisConfig.java
@@ -14,6 +14,7 @@ public class RedisConfig {
     @Bean
   //  @Profile("development")
     public SessionRepository<?> sessionRepository() {
+        //Session do Spring será mantida na memória da aplicação, substitui o spring.session.store-type=none q não está presente no SB3
         return new MapSessionRepository(new HashMap<String, Session>());
     }
 }
--- src/main/java/com/algaworks/algafood/core/security/ResourceServerConfig.java
+++ src/main/java/com/algaworks/algafood/core/security/ResourceServerConfig.java
@@ -13,7 +13,6 @@ import org.springframework.security.oauth2.server.resource.authentication.JwtGra
 import org.springframework.security.web.SecurityFilterChain;
 
 import java.util.Collection;
-import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
 import java.util.stream.Collectors;
@@ -23,7 +22,7 @@ import java.util.stream.Collectors;
 @EnableWebSecurity
 public class ResourceServerConfig {
 
-    @Bean
+    @Bean       //Este método é acessado todas vezes que é feito uma requisição na API
     public SecurityFilterChain resourceServerFilterChain(HttpSecurity http) throws Exception {
         http.formLogin(Customizer.withDefaults())   //Configuração necessária apenas se usar o AS dentro do Resource Server (a Rest API)
             .csrf().disable()
@@ -32,13 +31,12 @@ public class ResourceServerConfig {
                 .jwt()          //ResourceServer faz a leitura do tipo do token especificado para verificar se assinatura é válida
                 .jwtAuthenticationConverter(jwtAuthenticationConverter());
 
-        /*OBS: O retorno http do ResourceServerConfig precisa ser o IGUAL ao AuthorizationServerConfig, pois AS e RS estão integrados
-        Se as duas aplicações estiverem separadas, essa configuração do formLogin no ResourceServerConfig n é necessária*/
+        /*OBS: O retorno http do ResourceServerConfig precisa ser o IGUAL ao AuthorizationServerConfig, pois AS e RS estão integrados no mesmo projeto
+        Se as duas aplicações estiverem separadas, essa configuração do formLogin no ResourceServerConfig n seria necessária*/
         return http.build();
     }
 
-
-    //Lê  informações customizadas do JWT
+    //Lê informações customizadas do JWT
     private JwtAuthenticationConverter jwtAuthenticationConverter() {
         JwtAuthenticationConverter converter = new JwtAuthenticationConverter();
 
@@ -46,13 +44,14 @@ public class ResourceServerConfig {
             //"authorities" é um campo costumizado no AuthorizationServerConfig
             List<String> authorities = jwt.getClaimAsStringList("authorities");
 
-            if (Objects.isNull(authorities)) {          //Se for Client Critentials, a authorities é null
-                return Collections.emptyList();             //Evita erro de serialização durante a leitura do token
-            }
-
             //JwtGrantedAuthoritiesConverter converte os Scopes do JWT em Authority
             JwtGrantedAuthoritiesConverter authoritiesConverter = new JwtGrantedAuthoritiesConverter();
             Collection<GrantedAuthority> grantedAuthorities = authoritiesConverter.convert(jwt);
+
+            //Se for Client Critentials, a authorities é null. Logo, deve retornar grantedAuthorities que contém os SCOPES
+            if (Objects.isNull(authorities)) {
+                return grantedAuthorities;             //Evita erro de serialização durante a leitura do token
+            }
 
             //Combina as duas listas: a que vem dos scopes de JWT (grantedAuthorities) e as que vem dos "authorities" do JWT
             grantedAuthorities.addAll(authorities
